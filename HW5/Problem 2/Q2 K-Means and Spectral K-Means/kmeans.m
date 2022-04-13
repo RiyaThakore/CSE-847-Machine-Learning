aafb51feadbd691b54a628b705b41cf438591c40
Riya Thakore <30748253+RiyaThakore@users.noreply.github.com>
2022-04-13 17:42:09 -0400

    Update kmeans.m

--- HW5/Problem 2/Q2 K-Means and Spectral K-Means/kmeans.m
+++ HW5/Problem 2/Q2 K-Means and Spectral K-Means/kmeans.m
@@ -2,12 +2,12 @@ function [c, pt] = kmeans(d, k, iterations)
     [N, d] = size(d);
     centroid_indices = randperm(N, k);
     c = data(centroid_indices, :);
-    pt = get_membership(d, c);
+    pt = pt(d, c);
     errors = [];
     prev_error = -1;
     for i = 1:iterations
-        c = get_centroids(d, pt);
-        pt = get_membership(d, c);
+        c = centroids(d, pt);
+        pt = pt(d, c);
         err = sse(d, c, pt);
         if prev_error == err
             break
@@ -23,27 +23,26 @@ function error = sse(data, centroids, membership)
     [k, d] = size(centroids);
     dists = zeros(N, k);
     for group = 1:k
-        dists(:, group) = vecnorm(data - centroids(group, :), 1, 2);
+        dists(:, group) = vecnorm(data - c(group, :), 1, 2);
     end
 
     error = sum(membership .* dists, 'all');
 end
 
-function centroids = get_centroids(data, membership)
-    [N, d] = size(data);
-
-    centroids = ((membership' * data)' ./ sum(membership))';
+function c = centroids(d, pt)
+    [x,y] = size(d);
+    c = ((pt' * d)' ./ sum(pt))';
 end
 
-function membership = get_membership(data, centroids)
+function p = pt(data, centroids)
     [N, d] = size(data);
     [k, d] = size(centroids);
     dists = zeros(N, k);
     for group = 1:k
-        dists(:, group) = vecnorm(data - centroids(group, :), 1, 2);
+        dists(:, group) = vecnorm(data - c(group, :), 1, 2);
     end
     [values, idxs] = min(dists');
-    membership = zeros(N, k);
+    p = zeros(N, k);
     for i = 1:N
         membership(i, idxs(i)) = 1;
     end
