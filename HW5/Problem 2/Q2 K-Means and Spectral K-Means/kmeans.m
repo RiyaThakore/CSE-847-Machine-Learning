e89b07d54bbf88ed9d858c35503473d8128f9839
Riya Thakore <30748253+RiyaThakore@users.noreply.github.com>
2022-04-13 21:46:31 -0400

    Update kmeans.m

--- HW5/Problem 2/Q2 K-Means and Spectral K-Means/kmeans.m
+++ HW5/Problem 2/Q2 K-Means and Spectral K-Means/kmeans.m
@@ -1,49 +1,46 @@
-function [c, pt] = kmeans(d, k, iterations)
+function [c, pt] = kmeans(d, k, i)
     [N, d] = size(d);
-    centroid_indices = randperm(N, k);
-    c = data(centroid_indices, :);
+    idx = randperm(N, k);
+    c = data(idx, :);
     pt = pt(d, c);
-    errors = [];
+    err = [];
     prev_error = -1;
-    for i = 1:iterations
+    for i = 1:i
         c = centroids(d, pt);
         pt = pt(d, c);
-        err = sse(d, c, pt);
-        if prev_error == err
-            break
-        end
-        prev_error = err;
-        errors(end + 1) = err;
+        errors = sse(d, c, pt);
+        prev_error = errors;
+        err(end + 1) = errors;
     end
-    savefig clusters
 end
 
-function error = sse(data, centroids, membership)
-    [N, d] = size(data);
-    [k, d] = size(centroids);
+%membership%
+function p = pt(d, centroids)
+    [N, y] = size(d);
+    [k, y] = size(centroids);
     dists = zeros(N, k);
-    for group = 1:k
-        dists(:, group) = vecnorm(data - c(group, :), 1, 2);
+    for grp = 1:k
+        a(:, grp) = vecnorm(d - c(grp, :), 1, 2);
     end
-
-    error = sum(membership .* dists, 'all');
+    [val, idxs] = min(a');
+    p = zeros(N, k);
 end
 
+%error%
+function err = sse(d, centroids, m)
+    [x, y] = size(d);
+    [z, y] = size(centroids);
+    dists = zeros(x, z);
+    for grp = 1:z
+        a(:, grp) = vecnorm(d - c(grp, :), 1, 2);
+    end
+    err = sum(m .*a, 'all');
+end
+
+%finding centroids%
 function c = centroids(d, pt)
     [x,y] = size(d);
     c = ((pt' * d)' ./ sum(pt))';
 end
 
-function p = pt(data, centroids)
-    [N, d] = size(data);
-    [k, d] = size(centroids);
-    dists = zeros(N, k);
-    for group = 1:k
-        dists(:, group) = vecnorm(data - c(group, :), 1, 2);
-    end
-    [values, idxs] = min(dists');
-    p = zeros(N, k);
-    for i = 1:N
-        membership(i, idxs(i)) = 1;
-    end
-end
+
